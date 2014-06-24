5b014949c34424c47b6a04ed022422ac7ca07858
noahtreb <noahtreb@2e83d7bb-3a97-22bf-fd4c-fb2de1399740>
2014-06-24 14:47:40 +0000

    Added support for second bound handling method for refined forward and reverse simulations.

--- trunk/src/analysis.m
+++ trunk/src/analysis.m
@@ -43,6 +43,41 @@ for i = 1:numDataSavePts
     ylim([0 yMax]);
 end
 
+%% Produce initial plots of the data and absorbing current.
+figure;
+subplot(2, 2, 1);
+plot(t, squeeze(d1(speciesId, :, :)));
+ylim([0 yMax]);
+
+subplot(2, 2, 2);
+plot(t, squeeze(d2(speciesId, :, :)));
+ylim([0 yMax]);
+
+subplot(2, 2, 3);
+plot(t, a1(speciesId, :), 'or');
+
+subplot(2, 2, 4);
+plot(t, a2(speciesId, :), 'or');
+
+%% Produce refined plots of the data and absorbing current.
+
+for i = 1:numDataSavePts
+    figure;
+    subplot(2, 2, 1);
+    plot(t, squeeze(d3(speciesId, :, :, i)));
+    ylim([0 yMax]);
+    
+    subplot(2, 2, 2);
+    plot(t, squeeze(d4(speciesId, :, :, i)));
+    ylim([0 yMax]);
+    
+    subplot(2, 2, 3);
+    plot(t, squeeze(a3(speciesId, :, i)), 'or');
+    
+    subplot(2, 2, 4);
+    plot(t, squeeze(a4(speciesId, :, i)), 'or');
+end
+
 %% Produce initial plots of the absorbing current.
 
 figure;
@@ -51,3 +86,14 @@ plot(t, a1(speciesId, :), 'or');
 
 subplot(1, 2, 2);
 plot(t, a2(speciesId, :), 'or');
+
+%% Produce refined plots of the absorbing current.
+
+for i = 1:numDataSavePts
+    figure;
+    subplot(1, 2, 1);
+    plot(t, squeeze(a3(speciesId, :, i)), 'or');
+    
+    subplot(1, 2, 2);
+    plot(t, squeeze(a4(speciesId, :, i)), 'or');
+end
